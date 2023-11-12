1ec97c3c3f8ab9934b87249027ba4468c8ec178f
vaskatsantoni <vaskatsantoni@aueb.gr>
2023-11-12 17:04:40 +0200

    προσθήκη

---
+++ _pasted_code_/src/snippet/mvn.java
@@ -0,0 +1,10 @@
+package snippet;
+
+public class
+mvn io.quarkus.platform:quarkus-maven-plugin:3.5.0:create \
+-DprojectGroupId=gr.aueb \
+-DprojectArtifactId=edtmgr \
+-Dextensions='hibernate-orm,hibernate-orm-panache,jdbc-h2'
+
+
+

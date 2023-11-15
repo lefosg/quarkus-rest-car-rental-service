6ccb8fa58589a5992b28335e5a41cc78bd969fa7
Gobakos <dimitriskokkos98@gmail.com>
2023-11-15 19:34:26 +0000

    small fix

--- src/main/java/org/util/CurrencyConverter.java
+++ src/main/java/org/util/CurrencyConverter.java
@@ -9,19 +9,16 @@ public class CurrencyConverter {
     private static final double eur_to_usd = 1.10;
     private static final double usd_to_eur = 0.90;
 
-    public Money currencyConverter(Money money) {
+    public void currencyConverter(Money money) {
 
         if (money.getCurrency() == USD) {
             money.setAmount(money.getAmount()*usd_to_eur);
             money.setCurrency(EUR);
-            return money;
         };
         if (money.getCurrency() == EUR) {
             money.setAmount(money.getAmount()*eur_to_usd);
             money.setCurrency(USD);
-            return money;
-        };
-        return money;
+        }
 
     }
 }

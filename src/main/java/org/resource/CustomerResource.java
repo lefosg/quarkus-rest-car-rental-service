08db8315ca66d163b1c434876ad21c91d602af42
vaskatsantoni <vaskatsantoni@aueb.gr>
2023-12-15 00:13:45 +0200

    fail

--- src/main/java/org/resource/CustomerResource.java
+++ src/main/java/org/resource/CustomerResource.java
@@ -1,7 +1,37 @@
 package org.resource;
 
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.domain.Customer;
+import org.persistence.CustomerRepository;
 
-@Path("customer")
-public class CustomerResource {
-}
+import java.util.List;
+
+//@Path("customer")
+
+ //   @Consumes(MediaType.APPLICATION_JSON)
+ //   @Produces(MediaType.APPLICATION_JSON)
+//    @RequestScoped
+//    public class CustomerResource {
+ //       @Inject
+    //    CustomerRepository customerRepository;
+
+    //    @GET
+   //     @Transactional
+   //     public List<Customer> listAllCustomers() {
+     //       return customerRepository.listAll();
+    //    }
+
+     //   @GET
+//@Transactional
+     //   public String test() {
+     //       return "PELATES";
+    //    }
+ //   }
+
---
+++ src/test/java/org/resource/CustomerResourceTest.java
@@ -0,0 +1,21 @@
+//package org.resource;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.util.IntegrationBase;
+
+import static io.restassured.RestAssured.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@QuarkusTest
+//class CustomerResourceTest extends IntegrationBase {
+
+ //   @Test
+  //  void listAllCustomers() {
+     //   String p = when().get("/customer")
+      //          .then()
+      //          .extract().asString();
+
+    //    assertEquals("PELATES", p);
+  //  }
+//}

9181973d4f060dfc0f5490c9112ff94b7ed46df2
lefosg <lefterisgeo2001@gmail.com>
2023-12-14 23:18:23 +0200

    2 paths CompanyResource & Tests

--- src/main/java/org/representation/ChargingPolicyMapper.java
+++ src/main/java/org/representation/ChargingPolicyMapper.java
@@ -1,17 +1,17 @@
 package org.representation;
 
 import org.domain.ChargingPolicy;
-import org.domain.Company;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper(componentModel = "jakarta",
-        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
-        uses = ChargingPolicyMapper.class)
+@Mapper(
+    componentModel = "jakarta",
+    injectionStrategy = InjectionStrategy.CONSTRUCTOR
+)
 public abstract class ChargingPolicyMapper {
 
     public abstract ChargingPolicyRepresentation toRepresentation(ChargingPolicy chargingPolicy);
-    public abstract List<ChargingPolicyRepresentation> toRepresentationList(List<ChargingPolicy> chargingPolicy);
+    public abstract List<ChargingPolicyRepresentation> toRepresentationList (List<ChargingPolicy> chargingPolicies);
 }
--- src/main/java/org/representation/CompanyMapper.java
+++ src/main/java/org/representation/CompanyMapper.java
@@ -6,9 +6,11 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper(componentModel = "jakarta",
+@Mapper(
+    componentModel = "jakarta",
     injectionStrategy = InjectionStrategy.CONSTRUCTOR,
-    uses = ChargingPolicyMapper.class)
+    uses = ChargingPolicyMapper.class
+)
 public abstract class CompanyMapper {
 
     public abstract CompanyRepresentation toRepresentation(Company company);

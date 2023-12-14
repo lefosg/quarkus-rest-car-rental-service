b98bf23c789259c19189c43ab261968f3edfa209
vaskatsantoni <vaskatsantoni@aueb.gr>
2023-12-14 20:15:38 +0200

    repositories

--- src/main/java/org/persistence/RentRepository.java
+++ src/main/java/org/persistence/RentRepository.java
@@ -5,11 +5,10 @@ import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import io.quarkus.security.identity.request.UsernamePasswordAuthenticationRequest;
 import jakarta.enterprise.context.RequestScoped;
 import org.domain.Company;
+import org.domain.Customer;
 
 import java.util.List;
 
 @RequestScoped
-public class CompanyRepository implements PanacheRepositoryBase<Company, Integer> {
-
+public class RentRepository implements PanacheRepositoryBase<RentRepository, Integer>{
 }
-
--- src/main/java/org/persistence/TechnicalCheckReposotory.java
+++ src/main/java/org/persistence/TechnicalCheckReposotory.java
@@ -5,11 +5,9 @@ import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import io.quarkus.security.identity.request.UsernamePasswordAuthenticationRequest;
 import jakarta.enterprise.context.RequestScoped;
 import org.domain.Company;
+import org.domain.Customer;
 
 import java.util.List;
 
-@RequestScoped
-public class CompanyRepository implements PanacheRepositoryBase<Company, Integer> {
-
+public class TechnicalCheckReposotory implements PanacheRepositoryBase<TechnicalCheckReposotory,Integer>{
 }
-

4de59dd507ef50324cbfa38d6999babedf0bb205
vaskatsantoni <vaskatsantoni@aueb.gr>
2023-12-15 22:50:03 +0200

    Customer Map+Repository

--- src/main/java/org/persistence/CustomerRepository.java
+++ src/main/java/org/persistence/CustomerRepository.java
@@ -2,6 +2,7 @@ package org.persistence;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import io.quarkus.panache.common.Parameters;
 import io.quarkus.security.identity.request.UsernamePasswordAuthenticationRequest;
 import jakarta.enterprise.context.RequestScoped;
 import org.domain.Company;
@@ -11,4 +12,14 @@ import java.util.List;
 
 @RequestScoped
 public class CustomerRepository implements PanacheRepositoryBase<Customer,Integer> {
+    public List<Customer> findBySurname(String surname) {
+        if (surname == null)
+            return listAll();
+
+        return find("select customer from Customer customer where customer.surname = :surname",
+        Parameters.with("surname", surname).map()).list();
+    }
+
+
+
 }
--- src/main/java/org/representation/CustomerMapper.java
+++ src/main/java/org/representation/CustomerMapper.java
@@ -4,14 +4,30 @@ import org.domain.Company;
 import org.domain.Customer;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "jakarta",
 injectionStrategy = InjectionStrategy.CONSTRUCTOR,
 uses = CustomerMapper.class)
+
 public abstract class CustomerMapper {
 
     public abstract CustomerRepresentation toRepresentation(Customer customer);
     public abstract List<CustomerRepresentation> toRepresentationList(List<Customer> customer);
+
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "phone", target = "phone")
+    @Mapping(source = "city", target = "city")
+    @Mapping(source = "surname", target = "surname")
+    @Mapping(source = "number", target = "number")
+    @Mapping(source = "city", target = "city")
+    @Mapping(source = "street", target = "street")
+    @Mapping(source = "zipcode", target = "zipcode")
+    @Mapping(source = "holderName", target = "holderName")
+    @Mapping(source = "expirationDate", target = "expirationDate")
+    public abstract Customer toModel(CustomerRepresentation customerRepresentation);
 }

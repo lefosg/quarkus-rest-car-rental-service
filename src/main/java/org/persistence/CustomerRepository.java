9164723eef386fbc4162174daa2c905d7923643e
vaskatsantoni <vaskatsantoni@aueb.gr>
2023-12-18 21:11:57 +0200

    CustomerRepository

--- src/main/java/org/persistence/CustomerRepository.java
+++ src/main/java/org/persistence/CustomerRepository.java
@@ -12,12 +12,41 @@ import java.util.List;
 
 @RequestScoped
 public class CustomerRepository implements PanacheRepositoryBase<Customer,Integer> {
+
     public List<Customer> findBySurname(String surname) {
         if (surname == null)
             return listAll();
 
         return find("select customer from Customer customer where customer.surname = :surname",
         Parameters.with("surname", surname).map()).list();
+    }
+    public List<Customer> findByEmail(String email) {
+        if (email == null)
+            return listAll();
+
+        return find("select customer from Customer customer where customer.email = :email",
+                Parameters.with("email", email).map()).list();
+    }
+    public List<Customer> findByPhone(String phone) {
+        if (phone == null)
+            return listAll();
+
+        return find("select phone from Customer customer where customer.phone = :phone",
+                Parameters.with("phone", phone).map()).list();
+    }
+    public List<Customer> findByAFM(String AFM) {
+        if (AFM == null)
+            return listAll();
+
+        return find("select AFM from Customer customer where customer.AFM = :AFM",
+                Parameters.with("AFM", AFM).map()).list();
+    }
+    public List<Customer> findByName(String name) {
+        if (name == null || name.equals(""))
+            return listAll();
+
+        return find("select name from Customer customer where customer.name = :name",
+                Parameters.with("name", name).map()).list();
     }
 
 

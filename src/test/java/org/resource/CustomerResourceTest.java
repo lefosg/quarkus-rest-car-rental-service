a06df300d311bf691c9e95aa4598ebe7940ad3a5
Gobakos <dimitriskokkos98@gmail.com>
2023-12-22 17:08:28 +0200

    fixed some things

--- src/test/java/org/resource/CustomerResourceTest.java
+++ src/test/java/org/resource/CustomerResourceTest.java
@@ -2,13 +2,16 @@ package org.resource;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.representation.CompanyRepresentation;
 import org.representation.CustomerRepresentation;
 import org.util.IntegrationBase;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,6 +32,16 @@ class CustomerResourceTest extends IntegrationBase {
     }
 
     // ---------- PUT ----------
+    @Test
+    public void createNullId() {
+        CustomerRepresentation representation = createCustomerRepresentation(null);
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(representation)
+                .when().put("/customer/")
+                .then().statusCode(404);
+    }
 
 
     // ---------- DELETE ----------
@@ -45,13 +58,30 @@ class CustomerResourceTest extends IntegrationBase {
     }
 
     @Test
-    public void deleteOneCompany() {
+    public void deleteOneCustomer() {
         when().delete("/customer/" + custId)
             .then().statusCode(200);
         when().get("/customer/" + custId)
             .then().statusCode(404);
         when().get("/customer/" + 1005)  //1005 not in db
             .then().statusCode(404);
+    }
+    private CustomerRepresentation createCustomerRepresentation(Integer id) {
+        CustomerRepresentation representation = new CustomerRepresentation();
+        representation.id = id;
+        representation.name = "ΙΩΑΝΝΗΣ";
+        representation.email = "evangellou@gmail.com";
+        representation.password = "johnjohn";
+        representation.phone = "6941603677";
+        representation.street = "ΛΕΥΚΑΔΟΣ 22";
+        representation.city = "ΑΘΗΝΑ";
+        representation.zipcode = "35896";
+        representation.AFM = "166008282";
+        representation.surname = "ΕΥΑΓΓΕΛΟΥ";
+        representation.expirationDate = LocalDate.of(2027,11,26).toString();
+        representation.number = "7894665213797564";
+        representation.holderName = "ΙΩΑΝΝΗΣ ΕΥΑΓΓΕΛΟΥ";
+        return representation;
     }
 
 }

bfe1c30b0d6229bdbde78e9d8d24e2a1ed64d699
Gobakos <dimitriskokkos98@gmail.com>
2023-12-22 16:43:30 +0200

    customer

--- src/main/java/org/resource/CustomerResource.java
+++ src/main/java/org/resource/CustomerResource.java
@@ -6,11 +6,13 @@ import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
 import org.domain.Customer;
 import org.persistence.CustomerRepository;
 import org.representation.CustomerMapper;
 import org.representation.CustomerRepresentation;
 
+import java.net.URI;
 import java.util.List;
 
 @Path("customer")
@@ -40,6 +42,18 @@ public class CustomerResource {
    // }
 
     // ---------- PUT ----------
+    @PUT
+    @Transactional
+    public Response create(CustomerRepresentation representation) {
+        if (representation.id == null ||  customerRepository.findById(representation.id) != null ) {  //if id is null or already exists
+            throw new NotFoundException("[!] PUT /customer\n\tCould not create customer, invalid id");
+        }
+
+        Customer customer = customerMapper.toModel(representation);
+        customerRepository.persist(customer);
+        URI uri = UriBuilder.fromResource(CustomerResource.class).path(String.valueOf(customer.getId())).build();
+        return Response.created(uri).entity(customerMapper.toRepresentation(customer)).build();
+    }
 
 
 

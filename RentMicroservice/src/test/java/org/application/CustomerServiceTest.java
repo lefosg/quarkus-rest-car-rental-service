844fc3769d8ef4cbdb5fbcbb84d1897266f086a9
vginis <vginis10@gmail.com>
2024-04-07 13:29:37 +0300

    2 mockito tests i forgot

--- RentMicroservice/src/test/java/org/application/CustomerServiceTest.java
+++ RentMicroservice/src/test/java/org/application/CustomerServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.time.LocalDate;
+
 import static io.smallrye.common.constraint.Assert.assertFalse;
 import static io.smallrye.common.constraint.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,7 +19,8 @@ import static org.mockito.Mockito.when;
 public class CustomerServiceTest {
     Integer existingCustomer = 1000;
     Integer nonExistingCustomer = 4444;
-    CustomerRepresentation existingCustomerRepresentation;
+
+    CustomerRepresentation customer;
     @InjectMock
     UserManagementService userManagementService;
     @Inject
@@ -27,8 +30,8 @@ public class CustomerServiceTest {
     public void setup(){
         when(userManagementService.customerExists(existingCustomer)).thenReturn(true);
         when(userManagementService.customerExists(nonExistingCustomer)).thenReturn(false);
-//        existingCustomerRepresentation = userManagementService.customerById(existingCustomer);
-//        //when(existingCustomerRepresentation).thenReturn(userManagementService.customerById(existingCustomer));
+        customer = createCustomerRepresentation(existingCustomer);
+        when(userManagementService.customerById(existingCustomer)).thenReturn(customer);
     }
 
     @Test
@@ -36,20 +39,36 @@ public class CustomerServiceTest {
 
     @Test
     public void getNonExistingCustomer(){assertFalse(rentService.customerExist(nonExistingCustomer));}
-
-//   todo den ksero pos na to kano edo
     
-//    @Test
-//    public void getCustomerRepresentationTest(){
-//        CustomerRepresentation myCustomer = rentService.returnCustomerWithId(existingCustomer);
-//        assertEquals(existingCustomerRepresentation.id,myCustomer.id);
-//        assertEquals(existingCustomerRepresentation.name,myCustomer.name);
-//        assertEquals(existingCustomerRepresentation.holderName,myCustomer.holderName);
-//        assertEquals(existingCustomerRepresentation.surname,myCustomer.surname);
-//        assertEquals(existingCustomerRepresentation.street,myCustomer.street);
-//        assertEquals(existingCustomerRepresentation.email,myCustomer.email);
-//        assertEquals(existingCustomerRepresentation.AFM,myCustomer.AFM);
-//        assertEquals(existingCustomerRepresentation.city,myCustomer.city);
-//    }
+    @Test
+    public void getCustomerRepresentationTest(){
+        assertEquals(customer.id,rentService.returnCustomerWithId(existingCustomer).id);
+        assertEquals(customer.city,rentService.returnCustomerWithId(existingCustomer).city);
+        assertEquals(customer.street,rentService.returnCustomerWithId(existingCustomer).street);
+        assertEquals(customer.holderName,rentService.returnCustomerWithId(existingCustomer).holderName);
+        assertEquals(customer.surname,rentService.returnCustomerWithId(existingCustomer).surname);
+        assertEquals(customer.AFM,rentService.returnCustomerWithId(existingCustomer).AFM);
+        assertEquals(customer.email,rentService.returnCustomerWithId(existingCustomer).email);
+        assertEquals(customer.password,rentService.returnCustomerWithId(existingCustomer).password);
+    }
 
+        private CustomerRepresentation createCustomerRepresentation(Integer id) {
+        CustomerRepresentation representation = new CustomerRepresentation();
+        representation.id = id;
+        representation.name = "ΙΩΑΝΝΗΣ";
+        representation.email = "evangellou@gmail.com";
+        representation.password = "johnjohn";
+        representation.phone = "6941603677";
+        representation.street = "ΛΕΥΚΑΔΟΣ 22";
+        representation.city = "ΑΘΗΝΑ";
+        representation.zipcode = "35896";
+        representation.AFM = "166008282";
+        representation.surname = "ΕΥΑΓΓΕΛΟΥ";
+        representation.expirationDate = LocalDate.of(2027,11,26).toString();
+        representation.number = "7894665213797564";
+        representation.holderName = "ΙΩΑΝΝΗΣ ΕΥΑΓΓΕΛΟΥ";
+        return representation;
+    }
 }
+
+
--- RentMicroservice/src/test/java/org/application/RentServiceTest.java
+++ RentMicroservice/src/test/java/org/application/RentServiceTest.java
@@ -3,12 +3,18 @@ package org.application;
 import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
+import org.infastructure.service.fleet.representation.VehicleRepresentation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.util.Money;
+import org.util.VehicleState;
+import org.util.VehicleType;
 
 import static io.smallrye.common.constraint.Assert.assertFalse;
 import static io.smallrye.common.constraint.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 
 @QuarkusTest
@@ -16,6 +22,7 @@ public class RentServiceTest {
     Integer existingVehicle = 3005;
     Integer nonExistingVehicle = 4444;
 
+    VehicleRepresentation vehicle;
     @InjectMock
     FleetService fleetService;
     @Inject
@@ -25,6 +32,8 @@ public class RentServiceTest {
     public void setup(){
         Mockito.when(fleetService.vehicleExists(existingVehicle)).thenReturn(true);
         Mockito.when(fleetService.vehicleExists(nonExistingVehicle)).thenReturn(false);
+        vehicle = createVehicleRepresentation(existingVehicle);
+        when(fleetService.vehicleById(existingVehicle)).thenReturn(vehicle);
     }
 
     @Test
@@ -32,4 +41,31 @@ public class RentServiceTest {
 
     @Test
     public void getNonExistingVehicle(){assertFalse(rentService.rentedVehicleExist(nonExistingVehicle));}
+
+    @Test
+    public void getCustomerRepresentationTest(){
+        assertEquals(vehicle.id,rentService.returnVehicleWithId(existingVehicle).id);
+        assertEquals(vehicle.manufacturer,rentService.returnVehicleWithId(existingVehicle).manufacturer);
+        assertEquals(vehicle.model,rentService.returnVehicleWithId(existingVehicle).model);
+        assertEquals(vehicle.year,rentService.returnVehicleWithId(existingVehicle).year);
+        assertEquals(vehicle.miles,rentService.returnVehicleWithId(existingVehicle).miles);
+        assertEquals(vehicle.plateNumber,rentService.returnVehicleWithId(existingVehicle).plateNumber);
+        assertEquals(vehicle.vehicleState,rentService.returnVehicleWithId(existingVehicle).vehicleState);
+        assertEquals(vehicle.vehicleType,rentService.returnVehicleWithId(existingVehicle).vehicleType);
+        assertEquals(vehicle.fixedCharge,rentService.returnVehicleWithId(existingVehicle).fixedCharge);
+    }
+
+    private VehicleRepresentation createVehicleRepresentation(Integer id) {
+        VehicleRepresentation representation = new VehicleRepresentation();
+        representation.id = id;
+        representation.manufacturer = "AUDI";
+        representation.model = "A7";
+        representation.year = 2021;
+        representation.miles = 100000;
+        representation.plateNumber = "MMA-8745";
+        representation.vehicleType = VehicleType.Sedan;
+        representation.vehicleState = VehicleState.Available;
+        representation.fixedCharge = new Money(70);
+        return representation;
+    }
 }

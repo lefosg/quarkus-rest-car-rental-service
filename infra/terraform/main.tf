117218f938a219b35a1b1e297fe5b1da2830e12b
lefosg <47362786+lefosg@users.noreply.github.com>
2024-09-27 16:15:04 +0300

    wrong rg fix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -61,8 +61,8 @@ resource "azurerm_resource_group" "storage" {
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.storage.name
+  location                 = azurerm_resource_group.storage.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   public_network_access_enabled = true

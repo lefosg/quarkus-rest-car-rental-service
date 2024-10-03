0fad66517650a65da1a1e93058428e4f745194b4
lefosg <47362786+lefosg@users.noreply.github.com>
2024-10-03 14:22:38 +0300

    added helm char for microservices

--- infra/k8s/create-all.sh
+++ infra/k8s/create-all.sh
@@ -1,9 +1,14 @@
 # minikube addons enable ingress
+# istioctl install
+
+# kubectl create namespace ingress-nginx
+# kubectl create namespace argocd
+
+# kubectl label namespace ingress-nginx istio-injection=enabled
+# kubectl label namespace default istio-injection=enabled
+
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.34.1/deploy/static/provider/cloud/deploy.yaml
-
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl apply -f rent-msvc.yaml
 kubectl apply -f fleet-msvc.yaml
--- infra/k8s/delete-all.sh
+++ infra/k8s/delete-all.sh
@@ -1,13 +1,18 @@
 kubectl delete deployment fleet-deployment
 kubectl delete deployment rent-deployment
 kubectl delete deployment user-deployment
+kubectl delete deployment jaeger-deployment
 kubectl delete deployment hello-default-deployment
 
 kubectl delete svc default-http-backend
 kubectl delete svc fleet-service
 kubectl delete svc rent-service
 kubectl delete svc user-service
+kubectl delete svc jaeger-service
 
 kubectl delete ingress car-rental-service-ingress
 
 # kubectl delete namespace ingress-nginx
+# kubectl delete namespace argocd
+# kubectl label namespace ingress-nginx istio-injection-
+# kubectl label namespace default istio-injection-
